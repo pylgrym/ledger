bd518f633ed961d388fafdaca4c30065c82fe826
Jakob Gaardsted <jgaardsted@gmail.com>
2019-10-07 22:45:51 +0200

    fixes

--- src.lua
+++ src.lua
@@ -44,14 +44,29 @@ function handlers:PLAYER_ALIVE(...)	print("PA") end
 --MI2_EventHandlers["CHAT_MSG_COMBAT_XP_GAIN"] = MI2x_EventCreatureDiesXP
 --MI2_EventHandlers["CHAT_MSG_COMBAT_HOSTILE_DEATH"] = MI2x_CreatureDiesHostile
 
+-- /run print(GetItemInfo(12184))
+-- /run for i=1,150000,1 do print(i, GetItemInfo(i)) end
+-- /run print(GetItemInfo("Raptor Flesh"))
+-- /run for i=1,3,1 do print(i) end
+
 local function GetLootId( slot )
 	local idNumber = 0
 
 	local link = GetLootSlotLink( slot )
 	if link then
-		local _, _, idCode = string.find(link, "|Hitem:(%d+):(%d+):(%d+):")
+		local _, idCode = string.match(link, "^|(%x+)|Hitem:(%d+):.*")
 		idNumber = tonumber( idCode or 0 )
+		if idNumber == 0 then
+			local printable = gsub(link, "\124", "\124\124");
+			print('cant parse ', printable)
+			local a = GetItemInfo(link)
+			print('after loop')
+		end
+		print('after idn-zero test')
+	else 
+		print('link broken for slot', slot)
 	end
+	print('after link test')
 
 	return idNumber, link
 end -- GetLootId()
@@ -61,18 +76,43 @@ local corpseName = ""
 local corpseName2 = ""
 local theCorpseGuid = "" -- long (dont use unless you have to.)
 local corpseId = ""  -- short.
+local latestCombatMob = ""
 local lootCount = 0
 
-function handlers:COMBAT_LOG_EVENT(...) 
+--function handlers:COMBAT_LOG_EVENT(...)
+--	local ts,e = CombatLogGetCurrentEventInfo()
+--	print('CLE', e)
+--end
+
+function handlers:COMBAT_LOG_EVENT_UNFILTERED(...) 
+	--showArgs13("CLE", CombatLogGetCurrentEventInfo() )
 	-- den her gir et PARTY_KILL
-	local            ts,e,hideCaster,src_id,sname,srcFlags,sraidf,dst_id,dname,dFlags,draidf = CombatLogGetCurrentEventInfo()
+	local            ts,e,hideCaster,src_id,sname,srcFlags,sraidf,dst_id,dname,dstFlags,draidf = CombatLogGetCurrentEventInfo()
+	if false then
+		print('ts:',ts,
+			',e:',e,',hc:',hideCaster,
+			',sId:',src_id,',sN:',sname,',sF:',srcFlags,',sRF:',sraidf,
+			',dId:',dst_id,',dN:',dname,',dF:',dstFlags,',dRF:',draidf)
+	end
+
+	if false then -- annoying broken mechanism for CLE, when PARTY_KILL doesn't work.
+		--print(e)
+		if src_id then -- should be dst_id
+			local opponent = findMobId(src_id)
+			latestCombatMob = opponent
+			print('LCM:', latestCombatMob, src_id, e) --, 'from:'
+		else
+			print("src_id is bad for ", e)
+		end 
+	end 
+
 	if e == "PARTY_KILL" then 
 		state = "01_MOB_KILLED"
 		corpseName = dname
 		theCorpseGuid = dst_id
 		corpseId = findMobId(theCorpseGuid)
-		print("CLE:",ts,e,hideCaster,src_id,sname,srcFlags,sraidf,dst_id,dname,dFlags,draidf)
-		print("state",state, "with corpseId", corpseId)
+		print("CLEU:",ts,e,hideCaster,src_id,sname,srcFlags,sraidf,dst_id,dname,dFlags,draidf)
+		print("state",state, "with corpseId", corpseId, "corpseName:",corpseName)
     end
 end 
 
@@ -84,8 +124,8 @@ function handlers:PLAYER_TARGET_CHANGED(...)
 		-- fixme - check if target is dead.		
 		state = "02_TARGET_CORPSE"
 		lootCount=0
-		print("state 01->", state, "clearing lootcount:", lootCount)
 		corpseName2 = tname
+		print("state 01->", state, "clearing lootcount:", lootCount, "corpseName2:", corpseName2)
 	end
 
 	if tname == nil then tname = "<nil>" end
@@ -145,16 +185,25 @@ end
 lootMap = {} -- not local.
 
 function recordKill()	
+	print('recordKill', corpseId) --, ', ', latestCombatMob, '.')
 	--local inf = (lootMap[corpseId] and lootMap[corpseId] or {killCount=0, drops={})
 	if corpseId == nil then
-		print("no-corpse in record-kill")
-		return
+		if false then --latestCombatMob then
+			print("(record-kill NOC) - latestCombatMob instead: ", latestCombatMob)
+			corpseId = latestCombatMob
+			latestCombatMob = nil
+		else
+			print("no-corpse in record-kill, and no LCM :-/")
+			return
+		end 
 	end
 
-	if not lootMap[corpseId] then
+	if not lootMap[corpseId] then		
 	  lootMap[corpseId] = {killCount=0, drops={}, name=corpseName}
+	  print('creating mob with corpseName', corpseName)
 	end
 	local inf = lootMap[corpseId]
+	inf.name = corpseName -- just to repair.
 	inf.killCount = inf.killCount+1
 
 	local numItems = GetNumLootItems()
@@ -168,11 +217,15 @@ function recordKill()
 	if numItems == 0 then
 		local dropName = "nothing"
 		local amount = 1 -- one serving of 'nothing'
-		logItem(inf, dropName, amount) -- danger - pass-by-value?
+		local itemID = 0
+		logItem(inf, dropName, amount, itemID) -- danger - pass-by-value?
 	end
 
 	for slot = 1, numItems, 1 do
+		print('slot', slot, 'of', numItems)
 		local texture, iName, quantity, quality = GetLootSlotInfo( slot )
+		-- hmm, quality should be 'islocked'
+
 		local itemID, link = GetLootId( slot )
 		print("slot:",slot,
 			", tex:",texture,
@@ -192,26 +245,45 @@ function recordKill()
 			dropName="money"
 		end
 
-		logItem(inf, dropName, amount) -- danger - pass-by-value?
+		logItem(inf, dropName, amount, itemID) -- danger - pass-by-value?
 	end
-
+	print('after-loop')
 	lootMap[corpseId] = inf -- shouldnt be necessary?
 
 	printMobInf(inf,corpseId) --mobInf, mobId)
 
 	corpseId = nil -- avoid counting a kill more than once.
 	corpseName="<?>"
-
 end
 
-function logItem(inf, dropName, amount)
+
+function logItem(inf, dropName, amount, itemId)
 	if not inf.drops[dropName] then
-		inf.drops[dropName] = {name=dropName, count=0}
+		inf.drops[dropName] = {name=dropName, count=0, itemId = itemId, price=0}
 	end
 	local item = inf.drops[dropName]
-	item.count =item.count + amount
+	item.count = item.count + amount
+
+	print('itemPrice:', item.price, itemId, dropName)
+	if not (item.price>0) then -- (As long as price isn't set yet, keep trying to look it up.)
+		local price = getItemPrice(itemId)
+		if price then		
+			item.price = price
+		end
+	else
+		print('so ', item.price, 'is good enough')
+	end
+
 	inf.drops[dropName] = item -- shouldn't be necessary?
 	return inf -- could this help?
+end
+
+function getItemPrice(itemId)
+	local name, link, rarity, iLvl, iMinLvl, sType, sSubType, stack,loc,tx,price = GetItemInfo(itemId)
+	if price == nil then
+		price=0
+	end
+	return price
 end
 
 
@@ -222,9 +294,15 @@ function printLootMap()
 end
 
 function printMobInf(mobInf, mobId)
+	local total=0
 	for itemName,itemInfo in pairs(mobInf.drops) do
-		print(":", mobId, mobInf.name, itemName, "#",itemInfo.count, "/", mobInf.killCount)
+		local contribution = (itemInfo.price*itemInfo.count)
+		local sPrice = itemInfo.price*0.01
+		total = total + contribution
+		print(":", mobId, mobInf.name, "#",itemInfo.count, "/", mobInf.killCount, "$"..sPrice, itemName)
 	end
+	local totalAvg = total / mobInf.killCount
+	print('totalAvg(s):', totalAvg*0.01)
 end
 
 function handlers:LOOT_SLOT_CLEARED(...) 
@@ -251,7 +329,14 @@ end
 
 
 
-function handlers:CHAT_MSG_COMBAT_XP_GAIN(...) showArgs("CMCXG",...) end 
+--function handlers:CHAT_MSG_COMBAT_XP_GAIN(...) 
+	--local a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17 = ...
+	--print("CMCXG",a1,';',a2,';',a3,';',a4,';',a5,';',a6,';',a7,';',a8,';',a9,';',a10,';',a11,';',a12,';',a13,';',a14,';',a15,';',a16,';',a17, '!')
+	-- self becomes a table here - the 'handlers' is a table.
+	--showArgs("CMCXG", CombatLogGetCurrentEventInfo() ) 
+	-- FIXME - I need some args or info for this, it's useless?
+	-- Also - does gray-mob-level switch between CMCXG and PARTY_KILL?
+--end 
 --function handlers:CHAT_MSG_COMBAT_HOSTILE_DEATH(...) showArgs("CMCHD",...) end 
 
 
@@ -259,6 +344,12 @@ function showArgs(a,...)
 	local a1,a2,a3,a4,a5,a6,a7,a8,a9 = ...
 	print(self,a,a1,a2,a3,a4,a5,a6,a7,a8,a9)
 end
+
+function showArgs13(a,...)
+	local a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13 = ...
+	print(self,a,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13)
+end
+
 
 for k, v in pairs(handlers) do
  frame:RegisterEvent(k)
